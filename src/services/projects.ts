d215577d2702fd8f419e1b931be6cf95ad826cf0
Manuel <manuelcar96@outlook.com>
2025-04-03 22:26:39 -0500

    refactor: styles

---
+++ src/services/projects.ts
@@ -0,0 +1,34 @@
+export const projects = {
+  title: "Proyectos",
+  items: [
+    
+    {
+      title: "Ecommerce",
+      description: "Se hace ecommerce con Svelte, Tailwind y TypeScript",
+      image: "/img/Ecommerce.jpg",
+      tecnologies: ["Svelte", "Tailwind", "TypeScript", "MongoDB"],
+      link: "https://github.com/ManuPro14/ecommerce",
+    },
+    {
+      title: "Dictionary App",
+      description: "Aplicación de Diccionario de Ingles en NextJS, TypeScript y TailwindCSS",
+      image: "/img/dictionary-app.png",
+      tecnologies: ["NextJS", "TypeScript", "TailwindCSS"],
+      link: "https://dictionary-app-manu.vercel.app/",
+    },
+    {
+      title: "Clon Minecraft",
+      description: "Clon del juego Minecraft en ReactJS, CSS y HTML",
+      image: "/img/manucraft.jpg",
+      tecnologies: ["HTML", "CSS", "JavaScript"],
+      link: "https://manupro14.github.io/manucraft/",
+    },
+    {
+      title: "Landing Page",
+      description: "Landing page de la empresa MABANY GROUP",
+      image: "/img/Mabany.jpg",
+      tecnologies: ["Astro", "Tailwind", "TypeScript"],
+      link: "https://mabanygroupcom.netlify.app/",
+    },
+  ],
+};
